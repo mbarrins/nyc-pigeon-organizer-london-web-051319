acce999ccc4d16ddd9c199bee85a58012deb6735
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 19:37:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require "pry"
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  bird_hash = data.values[0].values.flatten.uniq.map{|bird| [bird, {}]}.to_h
+
+  bird_hash.keys.each do |bird|
+    data.keys.each{|type| bird_hash[bird][type] = data[type].select{|desc,names| names.include?(bird)}.keys.map(&:to_s)}
+  end
+  bird_hash
+
+
 end
+
+
+puts nyc_pigeon_organizer(pigeon_data)
