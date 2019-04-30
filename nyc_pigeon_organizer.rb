0ce30e0a5f52f78ef25d5e2723204a7201216184
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 19:37:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,13 +20,16 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-  bird_hash = data.values[0].values.flatten.uniq.map{|bird| [bird, {}]}.to_h
+  # bird_hash = data.values[0].values.flatten.uniq.map{|bird| [bird, {}]}.to_h
+  #
+  # bird_hash.keys.each do |bird|
+  #   data.keys.each{|type| bird_hash[bird][type] = data[type].select{|desc,names| names.include?(bird)}.keys.map(&:to_s)}
+  # end
+  # bird_hash
 
-  bird_hash.keys.each do |bird|
-    data.keys.each{|type| bird_hash[bird][type] = data[type].select{|desc,names| names.include?(bird)}.keys.map(&:to_s)}
-  end
-  bird_hash
-
+  data.values[0].values.flatten.uniq.map do |bird|
+      [bird, data.keys.map{|type| [type, data[type].select{|desc,names| names.include?(bird)}.keys.map(&:to_s)]}.to_h]
+  end.to_h
 
 end
 
